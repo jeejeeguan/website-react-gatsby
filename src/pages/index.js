2c50c96dbe1a26f2f15849998b42e8dbb5e1701c
jjkwan <jjkwanhere@gmail.com>
2020-09-11 08:01:28 +0800

    03.AdaptiveLayoutwithCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,13 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
+import Header from "../components/header";
+import App from "../components/animation";
 
 const IndexPage = () => (
   <Layout>
+    <Header />
+    <AniamtionLogo />
     <div className="Hero">
       <div className="HeroGroup">
         <SEO title="Home" />
@@ -15,7 +19,7 @@ const IndexPage = () => (
         <p>Welcome to your ne Gate.</p>
         <p>Now go build something great.</p>
         <Link to="/page-2/">Go to page 2</Link> <br />
-        {/* <Link to="/using-typescript/">"Using TypeScript"</Link> */}
+        <Link to="/using-typescript/">"Using TypeScript"</Link>
       </div>
     </div>
   </Layout>
